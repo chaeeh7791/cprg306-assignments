86bc6481b58451267341d0a6fe15e09b5780a429
Grace_Chae <eunhye.chae@edu.sait.ca>
2024-09-17 22:55:50 -0600

    Complete Assignment 2

---
+++ app/week-2/student-info.js
@@ -0,0 +1,14 @@
+import Link from "next/link";
+
+export default function StudentInfo() {
+  return (
+    <div>
+      <h1>Grace Chae</h1>
+      <li>
+        <Link href="https://github.com/chaeeh7791/cprg306-assignment.git">
+          GitHub Repository
+        </Link>
+      </li>
+    </div>
+  );
+}
